156e009d4d03f5b0cce6f7bdfd66708b1fdbfaee
Matheus-Herreira-Orestes <mherreirao@gmail.com>
2023-10-10 18:40:50 -0300

    Matérias prontas

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,15 +2,32 @@ export const dataFake = [
 
     {
         "id":"1",
-        "title":"Anunciada a dlc de mk1",
-        "description":"NetherRealm anuncia dlc de seu jogo, possuindo personagens famosos e queridos entre os fãs",
+        "title":"Mortal Kombat 1 Anuncia o Kombat Pack 1: Novos Personagens e Muito Mais!",
+        "description":"Mortal Kombat 1 Anuncia o Kombat Pack 1: Novos Personagens e Muito Mais! Os fãs de Mortal Kombat têm motivo para comemorar, pois a NetherRealm Studios anunciou recentemente o tão aguardado Kombat Pack 1 para Mortal Kombat 1. Este pacote de conteúdo adicional traz uma série de novidades empolgantes para o universo do jogo de luta mais icônico. Uma das maiores surpresas é a adição de novos personagens jogáveis. Entre eles, os fãs podem se preparar para lutar com Ermac, Homelander (Capitão Pátria, de The Boys), Omni-Man (de Invencível, da Image Comics), Peacemaker (Pacificador, da DC), Quan Chi, Takeda. Cada um desses lutadores traz seu próprio estilo de combate, movimentos especiais e Fatalities brutais, adicionando uma camada extra de diversidade ao elenco já impressionante de Mortal Kombat 1",
         "photoCover":"https://sm.ign.com/ign_br/screenshot/default/mortal-kombat-1-dlc_71gs.jpg"
     },
     {
         "id":"2",
-        "title":"mk1 lança",
-        "description":"aaaaaaaaaaaaaaaaaaaaaaaa",
+        "title":"Mortal Kombat 1 : A Lenda dos Jogos de Luta Está de Volta!",
+        "description":"Prepare-se para uma experiência totalmente nova e emocionante no mundo dos jogos de luta, já que a lendária franquia 'Mortal Kombat' está pronta para um reboot após os eventos de 'Mortal Kombat 11'. Os fãs estão curiosos para saber o desenrolar dos fatos na nova linha do tempo criada por Liu Kang após derrotar a Kronika.",
         "photoCover":"https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/software/switch/70070000017756/b17782f43960aea7f83e8a08a1244b9beed1e375fd8af8aa110398d5d3c930a4"
+    },
+
+    {
+        "id":"3",
+        "title":"Nova Temporada de Jujutsu Kaisen Encanta e Fideliza Fãs",
+        "description":"Desde a sua estreia, 'Jujutsu Kaisen' tem se destacado como uma das séries de anime mais populares e aclamadas dos últimos anos. O sucesso da primeira temporada deixou os fãs ansiosos por mais, e a nova temporada não apenas correspondeu às expectativas, mas também as superou. A segunda temporada da série demonstrou ser um verdadeiro presente para os fãs, conquistando ainda mais adeptos e mantendo os já existentes entusiastas da obra de Gege Akutami.",
+        "photoCover":"https://cdn.ome.lt/KyzgJm9Rd6aSWZbsIj4Mk1nm79o=/1200x630/smart/extras/conteudos/JujutsuKaisen_S2.png"
+    },
+
+    {
+        "id":"4",
+        "title":"Vasco da Gama Enfrenta Desafio Crucial para Evitar Queda à Série B",
+        "description":"O Club de Regatas Vasco da Gama, um dos clubes de futebol mais tradicionais do Brasil, está enfrentando mais um desafio crucial em sua história. Depois de uma passagem pela Série B do Campeonato Brasileiro em 2020, o clube carioca luta incansavelmente para evitar uma nova queda à segunda divisão e manter sua posição na elite do futebol brasileiro. Porem a luta não está nada fácil, a que o clube carioca se encontra na décima sétima (17) colocação na tabela do Brasileirão.",
+        "photoCover":"https://inmagazine.ig.com.br/public/assets/img/postagens/post_38597.jpg"
     }
+
+    
+
 
 ]
